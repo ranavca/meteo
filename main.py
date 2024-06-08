0d7f5f38c4a10c71430e0eecfd5502ff9a1996ed
Raimundo Navarrete Calderón <raymond@MacBook-Pro-de-Raimundo.local>
2024-06-08 15:48:20 -0400

    enable weather service

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-# from src.weather import Weather
+from src.weather import Weather
 from src.database import Database
 from flask_apscheduler import APScheduler
 import os
@@ -10,7 +10,7 @@ static_dir = os.path.abspath('./src/static/')
 app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
 scheduler = APScheduler()
 database = Database()
-# weather = Weather()
+weather = Weather()
 
 @app.route("/")
 def renderHome():
@@ -22,10 +22,10 @@ def initialize():
         database.save(random.randint(-10, 30), random.randint(1, 100), random.randint(956, 1012))
     return "Ready"
 
-# @scheduler.task('interval', id='save_meteo', seconds=10)
-# def saveWeatherValues():
-#     weatherValues = weather.getWeatherValues()
-#     database.save(weatherValues["temperature"], weatherValues["humidity"], weatherValues["pressure"])
+@scheduler.task('interval', id='save_meteo', seconds=10)
+def saveWeatherValues():
+    weatherValues = weather.getWeatherValues()
+    database.save(weatherValues["temperature"], weatherValues["humidity"], weatherValues["pressure"])
 
 if __name__ == '__main__':
     database.onStart()
