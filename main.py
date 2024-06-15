0c075d65ffd3eea11eeff050cb60d2f032c5322f
Raimundo Navarrete Calderón <raymond@MacBook-Pro-de-Raimundo.local>
2024-06-15 14:42:31 -0400

    dht fix

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-# from weather import Weather
+from weather import Weather
 from database import Database
 from flask_apscheduler import APScheduler
 import os
@@ -8,7 +8,7 @@ import random
 app = Flask(__name__)
 scheduler = APScheduler()
 database = Database()
-# weather = Weather()
+weather = Weather()
 
 @app.route("/")
 def renderHome():
@@ -20,10 +20,10 @@ def initialize():
         database.save(random.randint(-10, 30), random.randint(1, 100), random.randint(956, 1012))
     return "Ready"
 
-# @scheduler.task('interval', id='save_meteo', seconds=10)
-# def saveWeatherValues():
-#     weatherValues = weather.getWeatherValues()
-#     database.save(weatherValues["temperature"], weatherValues["humidity"], weatherValues["pressure"])
+@scheduler.task('interval', id='save_meteo', seconds=10)
+def saveWeatherValues():
+    weatherValues = weather.getWeatherValues()
+    database.save(weatherValues["temperature"], weatherValues["humidity"], weatherValues["pressure"])
 
 if __name__ == '__main__':
     database.onStart()
--- weather.py
+++ weather.py
@@ -1,16 +1,15 @@
 import adafruit_dht
+import board
 import BMP085 as BMP085
-
-DHT_PIN = 23
 
 class Weather:
     def __init__(self) -> None:
-        self.dht = adafruit_dht.DHT11(DHT_PIN)
+        self.dht = adafruit_dht.DHT11(board.D4)
         self.bmp = BMP085.BMP085()
 
     def getWeatherValues(self):
         return {
             "pressure": self.bmp.read_pressure(),
-            "temperature": self.dht.temperature(),
-            "pressure": self.dht.pressure(),
+            "temperature": self.dht.temperature,
+            "pressure": self.dht.pressure
         }
